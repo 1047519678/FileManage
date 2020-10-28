7c0bd4f9cfed01598df31c9645b66fb705970478
chen <cjj1047519678@users.noreply.github.com>
2020-10-28 15:02:54 +0800

    ...

---
+++ src/main/java/cn/mvc/service/HttpService.java
@@ -0,0 +1,18 @@
+package cn.mvc.service;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.ParseException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.net.URI;
+
+public interface HttpService {
+
+}
