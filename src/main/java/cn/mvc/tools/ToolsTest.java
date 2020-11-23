a4bb19f30a6b9e399999a8d9f85dff4f727027a2
chen <cjj1047519678@users.noreply.github.com>
2020-11-23 15:02:23 +0800

    ...

--- src/main/java/cn/mvc/tools/ToolsTest.java
+++ src/main/java/cn/mvc/tools/ToolsTest.java
@@ -183,7 +183,7 @@ public class ToolsTest {
 
     }
 
-    @Scheduled(cron = "0 0/1 * * * ?")
+    //@Scheduled(cron = "0 0/1 * * * ?")
     public void test() {
         // RedisTemplate调用
         System.out.println("start：测试1");
